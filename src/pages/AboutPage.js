ac2e68b7f0d85adea4771658be640c42e7061a5c
Jahid Eljah <jahideljah@gmail.com>
2023-02-26 23:44:53 +0100

    error, about & checkout pages setups

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from "../components";
 import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>;
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>Our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            corporis, veritatis, obcaecati velit sit eligendi consequatur nisi
+            corrupti, cum aliquid molestiae non provident dicta nihil sapiente
+            rerum dolore aspernatur error.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
 };
 
 const Wrapper = styled.section`

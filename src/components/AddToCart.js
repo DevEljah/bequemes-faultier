2f4bc29f8a63cb2c4be9f7afe6b00b5b6e67681d
Jahid Eljah <jahideljah@gmail.com>
2023-03-02 22:56:23 +0100

    Single Product - Add To Cart - Amount Buttons

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,6 +9,29 @@ const AddToCart = ({ product }) => {
   const { id, colors, stock } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+  // since there's no pint on adding "0" items to the cart
+  // by default it's going to be "1"
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
 
   return (
     <Wrapper>
@@ -30,7 +53,16 @@ const AddToCart = ({ product }) => {
           ))}
         </div>
       </div>
-      <div className="btn-container"> </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };

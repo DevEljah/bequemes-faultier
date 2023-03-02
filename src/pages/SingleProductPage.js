4825b614ad72b1a290a32c7e47e6ddc32e4a5acb
Jahid Eljah <jahideljah@gmail.com>
2023-03-02 14:54:38 +0100

    Single Product - Base Return

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -48,7 +48,51 @@ const SingleProductPage = () => {
     return <Error />;
   }
 
-  return <Wrapper>single product page</Wrapper>;
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviwes,
+    id: sku,
+    company,
+    images,
+  } = product;
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product /*={product}*/ />
+      <div className="section section-center page">
+        <Link to={"/products"} className="btn">
+          back to product
+        </Link>
+        <div className="products-center">
+          <ProductImages />
+          <section className="content">
+            {<h2> {name} </h2>}
+            <Stars className="price" />
+            <h5> {formatPrice(price)} </h5>
+            <p className="desc"> {description} </p>
+            <p className="info">
+              <span>Available :</span>
+              {stock > 0 ? "In Stock" : "out of Stock"}
+            </p>
+            <p className="info">
+              <span>SKU :</span>
+              {sku}
+            </p>
+            <p className="info">
+              <span> Brand </span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.main`

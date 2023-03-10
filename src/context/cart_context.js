a86f789e494d13e7dab3fcc99b58f7cbb1f4fafd
Jahid Eljah <jahideljah@gmail.com>
2023-03-10 18:50:09 +0100

    localStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,19 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+// check localStorage
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    // if there's (in cart localStorage) some item
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 699, //Cent
@@ -25,7 +36,10 @@ export const CartProvider = ({ children }) => {
     // color = mainColor / amount = amount (in AddToCart component)
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
-
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]); //everytime "state.cart" changes!
+  // console.log(state.cart);
   return (
     <CartContext.Provider value={{ ...state, addToCart }}>
       {children}
